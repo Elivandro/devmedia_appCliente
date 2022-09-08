81261a50b8fb837dbad9f25e30e6e695ebab5f2c
Elivandro <Elivandro@hotmail.com.br>
2022-09-08 19:39:24 -0400

    edit and delete buttons

--- resources/views/cliente/index.blade.php
+++ resources/views/cliente/index.blade.php
@@ -1,38 +1,55 @@
 @extends('layouts.app')
 @section('title', 'Listar clientes')
 @section('content')
-    <section>
-        <table class="table table-striped table-bordered table-hover w-75 mx-auto">
-            <thead class="table-dark">
-                <tr>
-                    <th>Nome</th>
-                    <th>Email</th>
-                    <th colspan="2">Endereço</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($clientes as $cliente)
-                    <tr>
-                        <td>{{ $cliente->nome }}</td>
-                        <td>{{ $cliente->email }}</td>
-                        <td>{{ $cliente->endereco }}</td>
-                        <td>
-                            <a href="#" class="btn btn-primary btn-sm">Editar</a>
-                            <a href="#" class="btn btn-secondary btn-sm">Excluir</a>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-            <tfoot>
-                <tr>
-                    <td colspan="4">
-                        <div class="pagination d-flex justify-content-center">
-                            {{ $clientes->links('pagination::bootstrap-4') }}
-                        </div>
-                    </td>
-                </tr>
-            </tfoot>
-        </table>
-
+<section class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <ol class="breadcrumb m-0 p-0">
+                            <li class="breadcrumb-item active">Clientes</li>
+                            <li class="breadcrumb-item"><a href="{{ route('clientes.create') }}">Adcionar</a></li>
+                        </ol>
+                    </div>
+                    <div class="card-body">
+                        <table class="table table-striped table-bordered table-hover">
+                            <thead class="table-dark">
+                                <tr>
+                                    <th>Nome</th>
+                                    <th>Email</th>
+                                    <th colspan="2">Endereço</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($clientes as $cliente)
+                                    <tr>
+                                        <td>{{ $cliente->nome }}</td>
+                                        <td>{{ $cliente->email }}</td>
+                                        <td>{{ $cliente->endereco }}</td>
+                                        <td>
+                                            <form action="{{ route('clientes.destroy', $cliente->id) }}" method="post">
+                                                @method('DELETE')
+                                                @csrf
+                                                <a href="{{ route('clientes.edit', $cliente->id) }}" class="btn btn-outline-primary btn-sm">Editar</a>
+                                                <button class="btn btn-outline-secondary btn-sm">Excluir</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <td colspan="4">
+                                        <div class="pagination justify-content-center">
+                                            {{ $clientes->links('pagination::bootstrap-4') }}
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 @endsection
